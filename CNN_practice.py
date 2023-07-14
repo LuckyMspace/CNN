bbb7189058527df7a070c01a2d2ac3a940a3f7a7
LuckyMspace <119780984+LuckyMspace@users.noreply.github.com>
2023-07-14 23:58:14 +0900

    Update CNN_practice.py

--- CNN_practice.py
+++ CNN_practice.py
@@ -139,3 +139,56 @@ class FashionCNN(nn.Module):
             out = self.fc3(out)
             return out
 
+# 합성곱 네트워크를 위한 파라미터 정의
+learning_rate = 0.001
+model = FashionCNN()
+model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+print(model)
+
+# 모델 학습 및 성능 평가
+num_epochs = 5
+count = 0
+loss_list = []
+iteration_list = []
+accuracy_list = []
+
+predictions_list = []
+labels_list = []
+
+for epoch in range(num_epochs):
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        train = Variable(images.view(100, 1, 28, 28))
+        labels = Variable(labels)
+
+        outputs = model(train)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        count += 1
+
+        if not(count % 50):
+            total = 0
+            correct = 0
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                labels_list.append(labels)
+                test = Variable(images.view(100, 1, 28, 28))
+                outputs = model(test)
+                predictions = torch.max(outputs, 1)[1].to(device)
+                predictions_list.append(predictions)
+                correct += (predictions == labels).sum()
+                total += len(labels)
+
+            accuracy = correct * 100 / total
+            loss_list.append(loss.data)
+            iteration_list.append(count)
+            accuracy_list.append(accuracy)
+
+        if not (count % 500):
+            print("Iteration : {}, Loss: {}, Accuracy: {}%".format(count, loss.data, accuracy))
